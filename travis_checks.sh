a8c3962707f887818ebd318b0c65dfdc8a38feb3
alexv <asmodehn@gmail.com>
2016-11-14 16:48:36 +0900

    Rebase from 'upstream'

--- travis_checks.sh
+++ travis_checks.sh
@@ -18,5 +18,14 @@ if [ "$ROS_FLOW" == "devel" ]; then
 elif [ "$ROS_FLOW" == "install" ]; then
     make -j1 install
     source install/setup.bash
+    # CAREFUL these need to match the tests in CMakeLists.txt
+    #python -m pytest --pyargs tests/test_aiohttpparser.py
+    python -m pytest --pyargs tests/test_bottleparser.py
+    python -m pytest --pyargs tests/test_core.py
+    #python -m pytest --pyargs tests/test_djangoparser.py
+    #python -m pytest --pyargs tests/test_falconparser.py
+    python -m pytest --pyargs tests/test_pyramidparser.py
+    python -m pytest --pyargs tests/test_tornadoparser.py
+    #python -m pytest --pyargs tests/test_webapp2parser.py
     python -m pytest --pyargs webargs
 fi
